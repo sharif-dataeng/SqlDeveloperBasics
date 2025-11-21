9bda53742ee85fd803a3c01c46d571a60530c4f3
akhilpacha399-tech <akhil.pacha399@gmail.com>
2025-11-21 12:08:03 +0530

    Update 020.CreateTableWithCheckConstraint2.sql

--- sql-basics/020.CreateTableWithCheckConstraint2.sql
+++ sql-basics/020.CreateTableWithCheckConstraint2.sql
@@ -1,27 +1,22 @@
 
 
-CREATE TABLE EMPLOYEES6(
+CREATE TABLE EMPLOYEES8(
 	EmployeeId int,
 	EmployeeName varchar(100),
 	DOJ datetime,
-	Salary float check(salary>=10000),
-	Gender char(1) check(Gender = 'M' or Gender = 'F' or Gender = 'O') 
-	--Gender char(1) check(Gender IN('M','F','O')) 
-)
+	Salary float CHECK (Salary >= 10000),
+	Gender char(1) CHECK (Gender IN ('M','F'))
+);
 
-DROP TABLE EMPLOYEES6
-
-INSERT INTO EMPLOYEES6(EmployeeId,EmployeeName,DOJ,Salary,Gender) 
-VALUES(1,'JACK','2022-11-24',30000,'M')
+INSERT INTO EMPLOYEES8(EmployeeId,EmployeeName,DOJ,Salary,Gender)
+VALUES (1, 'JACK', '2022-11-24', 30000, 'M');
 
 
-INSERT INTO EMPLOYEES6(EmployeeId,EmployeeName,DOJ,Salary,Gender) 
-VALUES(2,'AMANDA','2022-11-24',30000,'F')
+INSERT INTO EMPLOYEES8(EmployeeId,EmployeeName,DOJ,Salary,Gender)
+VALUES (2, 'AMANDA', '2022-11-24', 30000, 'F');
 
+INSERT INTO EMPLOYEES8(EmployeeId,EmployeeName,DOJ,Salary,Gender)
+VALUES (3, 'lokesh', '2022-11-24', 30000, 'M');
 
-
-INSERT INTO EMPLOYEES6(EmployeeId,EmployeeName,DOJ,Salary,Gender) 
-VALUES(3,'XYZ','2022-11-24',30000,'O')
-
-INSERT INTO EMPLOYEES6(EmployeeId,EmployeeName,DOJ,Salary,Gender) 
-VALUES(4,'XYZ','2022-11-24',30000,'X')
+INSERT INTO EMPLOYEES8(EmployeeId,EmployeeName,DOJ,Salary,Gender)
+VALUES (4, 'swetha', '2022-11-24', 30000, 'F');
