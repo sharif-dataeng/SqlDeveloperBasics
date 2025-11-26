54d59acc188d91fdf0f93a2cd0aedf7adb9ff4b3
akhilpacha399-tech <akhil.pacha399@gmail.com>
2025-11-26 19:02:31 +0530

    Update 037.Datatypes-Part5-bit.sql

--- sql-basics/037.Datatypes-Part5-bit.sql
+++ sql-basics/037.Datatypes-Part5-bit.sql
@@ -1,23 +1,18 @@
 
 
-CREATE TABLE tblemps(
 CREATE TABLE emps(
 	empid int,
 	empname varchar(100),
 	isactive bit
 )
 
-insert into tblemps(empid,empname,isactive)values(1,'john',1)
 insert into emps(empid,empname,isactive)values(1,'alice',0)
 
-insert into tblemps(empid,empname,isactive)values(2,'jack',0)
 insert into emps(empid,empname,isactive)values(5,'robert',1)
 
 
-insert into tblemps(empid,empname,isactive)values(3,'yusuf','True')
 insert into emps(empid,empname,isactive)values(3,'sarah',1)
 
-insert into tblemps(empid,empname,isactive)values(4,'sam','False')
 insert into emps(empid,empname,isactive)values(7,'mike',0)
 
-select * from tblempsselect * from emps
+select * from emps
