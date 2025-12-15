26dba975711097c079830a2ef6807901cbcb20bb
akhilpacha399-tech <akhil.pacha399@gmail.com>
2025-12-15 13:27:33 +0530

    Create CREATE TABLE Customers (.sql

---
+++ sql-basics/CREATE TABLE Customers (.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    City VARCHAR(50)
+);
+
+INSERT INTO Customers (CustomerID, Name, City) VALUES
+(1, 'Alice', 'Delhi'),
+(2, 'Bob', 'Mumbai'),
+(3, 'Charlie', 'Bangalore');
