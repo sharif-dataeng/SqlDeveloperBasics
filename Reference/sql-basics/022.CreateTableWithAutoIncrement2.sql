cbf8a069d6eb31077481a00ea7b49f7f41c6cb5a
akhil <akhilpacha3939@gmail.com>
2026-01-24 13:58:52 +0530

    auto increment

--- Reference/sql-basics/022.CreateTableWithAutoIncrement2.sql
+++ Reference/sql-basics/022.CreateTableWithAutoIncrement2.sql
@@ -1,15 +1,23 @@
+DROP TABLE IF EXISTS EMPLOYEES_AutoIncrement2;
+GO
 
 CREATE TABLE EMPLOYEES_AutoIncrement2(
-	EmployeeId int primary key identity(100,1),
+	EmployeeId int primary key identity(100,2),
 	EmployeeName varchar(100),
 	DOJ datetime,
 	Salary float
 )
 
-select * from EMPLOYEES_AutoIncrement2
+
 
 INSERT INTO EMPLOYEES_AutoIncrement2(EmployeeName,DOJ,Salary) 
 VALUES('JACK','2022-11-24',30000)
 
 INSERT INTO EMPLOYEES_AutoIncrement2(EmployeeName,DOJ,Salary) 
 VALUES('JACKson','2023-11-24',30320)
+
+INSERT INTO EMPLOYEES_AutoIncrement2(EmployeeName,DOJ,Salary) 
+VALUES('JACKie','2024-11-24',30500)
+
+select * from EMPLOYEES_AutoIncrement2
+GO
