1a653efb63ae452f707d6478ff0b7ae84e1ddbdd
sharif-dataeng <sharif.dataeng@gmail.com>
2026-01-27 16:56:45 +0530

    Add mock data scripts for Person and Customer tables
    Added SQL scripts to insert 50 mock rows each into Person.Person, Person.PersonPhone, and Sales.Customer tables for testing and development. Also updated dataload_control.sql with commented update statements for loadtype and isactive fields.

---
+++ sharif/insert_person_personphone_mockdata.sql
@@ -0,0 +1,62 @@
+-- Mock data inserts for Person.PersonPhone (50 rows)
+-- Columns: BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate
+SET NOCOUNT ON;
+BEGIN TRANSACTION;
+
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (1, '555-0001', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (2, '555-0002', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (3, '555-0003', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (4, '555-0004', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (5, '555-0005', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (6, '555-0006', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (7, '555-0007', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (8, '555-0008', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (9, '555-0009', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (10, '555-0010', 1, GETDATE());
+
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (11, '555-0011', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (12, '555-0012', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (13, '555-0013', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (14, '555-0014', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (15, '555-0015', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (16, '555-0016', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (17, '555-0017', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (18, '555-0018', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (19, '555-0019', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (20, '555-0020', 2, GETDATE());
+
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (21, '555-0021', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (22, '555-0022', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (23, '555-0023', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (24, '555-0024', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (25, '555-0025', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (26, '555-0026', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (27, '555-0027', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (28, '555-0028', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (29, '555-0029', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (30, '555-0030', 3, GETDATE());
+
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (31, '555-0031', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (32, '555-0032', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (33, '555-0033', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (34, '555-0034', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (35, '555-0035', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (36, '555-0036', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (37, '555-0037', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (38, '555-0038', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (39, '555-0039', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (40, '555-0040', 1, GETDATE());
+
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (41, '555-0041', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (42, '555-0042', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (43, '555-0043', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (44, '555-0044', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (45, '555-0045', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (46, '555-0046', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (47, '555-0047', 2, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (48, '555-0048', 3, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (49, '555-0049', 1, GETDATE());
+INSERT INTO Person.PersonPhone (BusinessEntityID, PhoneNumber, PhoneNumberTypeID, ModifiedDate) VALUES (50, '555-0050', 2, GETDATE());
+
+COMMIT TRANSACTION;
+PRINT 'Inserted 50 mock Person.PersonPhone rows.';
