07c69248ca20b7cb278dd87d24086ebbfe31fcac
aliya6578 <138955322+aliya6578@users.noreply.github.com>
2026-01-28 11:31:03 +0530

    practice

--- aliya/practice_150126.sql
+++ aliya/practice_150126.sql
@@ -1,1 +1,8 @@
-select * from employees
+CREATE TABLE Customer(
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Country VARCHAR(50),
+    Age INT CHECK (Age >= 0 AND Age <= 99),
+    Phone int(10)
+);
