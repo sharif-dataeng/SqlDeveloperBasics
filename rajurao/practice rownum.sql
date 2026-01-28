70051a2cb1d3c1eed1da1172c0468459fc76899a
rajurao-t <rajurao.dataeng@gmail.com>
2026-01-28 10:40:55 +0530

    rownumber.sql

---
+++ rajurao/practice rownum.sql
@@ -0,0 +1,37 @@
+
+
+drop table employees
+
+CREATE TABLE EMPLOYEES(
+	EmployeeId int primary key identity(1,1),
+	EmpName varchar(100),
+	City varchar(30),
+	Department varchar(50),
+	Salary money
+)
+INSERT INTO EMPLOYEES(EmpName,City,Department,Salary)VALUES
+('John','NYC','IT',40000),
+('Amanda','London','Sales',40000),
+('Aman','Pune','IT',30000),
+('Rahul','Pune','IT',26000),
+('Sam','London','Sales',30000),
+('Rakesh','Delhi','IT',20000),
+('Suraj','Pune','Sales',12000),
+('Ajay','Delhi','Sales',10000),
+('Ankita','Delhi','Sales',10000),
+('Sudeep','Delhi','IT',36000),
+('Sanket','Pune','IT',40000)
+
+select * from employees
+
+SELECT EmpName, City, Department,Salary,
+ROW_NUMBER() OVER(ORDER BY salary desc) RowNum
+FROM EMPLOYEES
+
+SELECT EmpName, City, Department,Salary,
+ROW_NUMBER() OVER(PARTITION BY CITY ORDER BY EmpName) RowNum
+FROM EMPLOYEES
+
+SELECT EmpName, City, Department,Salary,
+ROW_NUMBER() OVER(PARTITION BY CITY,Department ORDER BY EmpName) RowNum
+FROM EMPLOYEES
