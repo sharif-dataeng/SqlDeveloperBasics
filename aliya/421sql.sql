de370ba46ea45c2a080b05182c373c85a45cc404
aliya6578 <138955322+aliya6578@users.noreply.github.com>
2026-02-03 19:26:13 +0530

    Create 421sql.sql
    created table

---
+++ aliya/421sql.sql
@@ -0,0 +1,12 @@
+create table empnew1
+(id int,
+name varchar(60),
+salary float,
+email varchar(70)
+);
+insert into empnew1 values(1,'aliya',20000,'aliya@gmail.com')
+insert into empnew1 values(2,'shravya',25000,'shravya@gmail.com')
+
+select * from empnew1;
+
+update empnew set salary = 4000 where id=1 or id=
